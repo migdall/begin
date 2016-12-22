635a5dd541944be5f71fe97748e1264dcf53f939
Jesus Noland <jn@jesusnoland.com>
2016-12-22 00:45:01 -0800

    Untracking the begin.py file

--- begin/begin.py
+++
@@ -1,8 +0,0 @@
-# The beginning of the program
-
-from entitymanager import EntityManager
-
-print ("Welcome to the program")
-
-entity_manager = EntityManager()
-print ("Entity created:", entity_manager.createEntity())
