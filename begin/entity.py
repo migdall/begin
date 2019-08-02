f2479ebd48db7e822611f99e0a4b82ce769eb1c9
Jesus Noland <jn@jesusnoland.com>
2019-08-01 21:49:51 -0700

    Using uuid instead of random package inside of the Entity class

--- begin/entity.py
+++ begin/entity.py
@@ -1,14 +1,17 @@
 # Entity class
 
-# Need random to generate an Entity's id property
-import random
+# Need unique identifier an Entity's id property if not given
+import uuid
 
 class Entity:
     ''' An Entity is literally just an integer ID '''
     
-    def __init__(self, id):
+    def __init__(self, id=None):
         ''' Initialize a new Entity with an id number or it will randomly choose a number between 0 and one million '''
-        self.id = id
+        if id:
+            self.id = id
+        else:
+            self.id = uuid.uuid4().hex
         
     def get_id(self):
         ''' Return the id of the Entity object '''
